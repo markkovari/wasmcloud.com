e14db2c8e2b75b9882c2f49e7265c6e0f9826930
markkovari <kovarimarkofficial@gmail.com>
2025-07-24 18:15:00 +0200

    chore: bump github stargazer count to 2000 to make the diff smaller
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -211,7 +211,7 @@ const config = (async (): Promise<Config> => {
             href: 'https://github.com/wasmcloud/wasmcloud',
             'aria-label': 'Star wasmCloud on GitHub',
             position: 'right',
-            html: `<span class="badge badge--outline">Star us! ★ <github-count repo="wasmcloud/wasmcloud">1500</github-count></span>`,
+            html: `<span class="badge badge--outline">Star us! ★ <github-count repo="wasmcloud/wasmcloud">2000</github-count></span>`,
             className: 'sidebar-hidden',
           },
           await svgIconNavItem({
